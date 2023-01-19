c4e80b6d1cb26e5ecdc47a3446c8f810009eb8c9
Alex Velazquez <arichard.1190@gmail.com>
2023-01-19 16:12:29 -0300

    Primer Commit

---
+++ FiveHeads.js
@@ -0,0 +1,30 @@
+function tossCoin() {
+    return Math.random() > 0.5 ? "heads" : "tails";
+}
+
+
+function fiveHeads() {
+    return new Promise( (resolve, reject) => {
+        // tu código aquí!
+        let headsCount = 0;
+        let attempts = 0;
+        while(headsCount < 5) {
+            attempts++;
+            let result = tossCoin();
+            console.log(`${result} was flipped`);
+            if(result === "heads") {
+                headsCount++;
+            } else {
+                headsCount = 0;
+            }
+            if(attempts === 20) reject(`5 consecutive "heads" were not reached`);
+        }
+        //las veces que salió cara
+        resolve(`It took ${attempts} tries to flip five "heads"`);
+        
+    });
+}
+fiveHeads()
+    .then( res => console.log(res) )
+    .catch( err => console.log(err) );
+console.log( "When does this run now?" );
